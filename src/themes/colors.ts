dcdccddcce66ab4a0aadfa4b25741597bcf674c8
Areydra Desfikriandre <areydra@gmail.com>
2023-04-26 10:16:43 +0700

    add new colors file

---
+++ src/themes/colors.ts
@@ -0,0 +1,25 @@
+export default {
+    brand: {
+        dark: '#001A83',
+        default: '#002DE3',
+        darkMode: '#375FFF',
+        light: '#879FFF',
+        background: '#D2D5F9',
+    },
+    neutral: {
+        active: '#0F1828',
+        dark: '#152033',
+        body: '#1B2B48',
+        weak: '#A4A4A4',
+        disabled: '#ADB5BD',
+        line: '#EDEDED',
+        offWhite: '#F7F7FC',
+        white: '#FFFFFF',
+    },
+    accent: {
+        danger: '#E94242',
+        warning: '#FDCF41',
+        success: '#2CC069',
+        safe: '#7BCBCF',
+    },
+};
